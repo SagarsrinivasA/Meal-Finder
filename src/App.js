a218a0ed3bfc50fe06663fcf8f179446f6c16d29
Your Name <you@example.com>
2022-03-30 00:29:10 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Header } from './Component/Header';
+import  From  from './Component/From';
+
+function App() {
+  return (
+   <section>
+     <div className='container'>
+       <Header/>
+       <From/>
+     </div>
+   </section>
+  );
+}
+
+export default App;
